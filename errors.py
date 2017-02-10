5db63064a83b474b2eae523220b2db4e7c23f1d9
Andrew Che <andrew@codeninja55.me>
2017-02-10 14:29:44 +1100

    Refactored error codes
    - Refactored error code messages to print based on the type pass in.
    - Also added the input argument to print message to player.

--- errors.py
+++ errors.py
@@ -2,65 +2,51 @@ from helpers import clear
 
 
 class InputError:
-    def index_selection(self):
+    def __init__(self, type, inpt):
         clear()
         print("*" * 60)
-        print("Your choice is not available as an index.")
-        print("Try again...")
-        print("*" * 60)
-
-    def value(self):
-        clear()
-        print("*" * 60)
-        print("You have not put an integer number.")
-        print("Try again...")
-        print("*" * 60)
-
-    def direction(self):
-        clear()
-        print("*" * 60)
-        print("You have not selected a correct value for direction.")
-        print("Try again...")
-        print("*" * 60)
-
-    def coordinates(self):
-        clear()
-        print("*" * 60)
-        print("You have not put a correct coordinates value number.")
-        print("e.g. a1 or a10")
-        print("Try again...")
-        print("*" * 60)
-
-    def confirmation(self):
-        clear()
-        print("*" * 60)
-        print("You have not made the right selection.")
-        print("Try again...")
-        print("*" * 60)
-
-    def guess(self):
-        clear()
-        print("*" * 60)
-        print("Your guess coordinates are invalid.")
-        print("Try again...")
+        print("You have entered: {}".format(str(inpt).upper()))
+        if type == 'index_selection':
+            print("Your choice is not available as an index.")
+        elif type == 'value':
+            print("You have not put an integer number.")
+        elif type == 'orientation':
+            print("You have not selected a correct value for orientation.")
+        elif type == 'coordinates':
+            print("You have not put a correct coordinates value number.")
+        elif type == 'coordinate_int':
+            print("Your coordinates second value is not an integer")
+        elif type == 'coordinate_ord':
+            print("Your coordinates second value is not a a letter")
+        elif type == 'board_boundaries':
+            print("Your coordinates are not within the boundaries of the board")
+        elif type == 'guess':
+            print("Your guess coordinates are invalid.")
+        elif type == 'invalid_str':
+            print("Your guess coordinates does not start with a letter.")
+        elif type == 'guessed_already':
+            print("You have already made that guess.")
+        elif type == 'confirmation':
+            print("You have not made the right selection.")
+        print("\nTry again...")
         print("*" * 60)
 
 
 class ShipPlacementError:
-    # print out error message
-    def __init__(self):
-        clear()
-        print("*" * 100)
-        print("Your placement choice puts the battleship out of bounds of "
-              "the board")
-        print("Try again...")
-        print("*" * 100)
-
-
-class CoordinatesOccupiedError:
-    def __init__(self):
+    def __init__(self, type, inpt_coord, inpt_orientation):
         clear()
         print("*" * 60)
-        print("These coordinates are occupied by another ship.")
+        print("You have entered: {}\n"
+              "Orientation: {}".format(inpt_coord.upper(),
+                                       inpt_orientation.upper()))
+
+        if type == 'outofbounds':
+            print("Your placement choice puts the battleship out of bounds of "
+              "the board")
+        elif type == 'occupied':
+            print("These coordinates are occupied by another ship.")
+        else:
+            print("Unknown Ship Placement Error")
+
         print("Try again...")
         print("*" * 60)
