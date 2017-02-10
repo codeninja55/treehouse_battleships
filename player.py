04ac226cf90c3b14c05b5e1e14252d579b803798
Andrew Che <andrew@codeninja55.me>
2017-02-10 14:30:24 +1100

    Refactored all input calls with recursion

--- player.py
+++ player.py
@@ -1,39 +1,38 @@
 from battleship import *
 from board import Board
 from errors import InputError
-from helpers import clear
+from helpers import clear, validate_coord
 
 
 class Player:
     def ship_placement(self):
         while len(self.battleship.ship_info) != 0:
-            self.player_board.print_board(self.player_board.game_board)
 
             selection = self.get_selection()
             coord = self.get_coordinates()
-            direction = self.get_direction()
+            orientation = self.get_orientation()
 
             # The condition will test if the ship can be placed on the board
             # if its inside the bounds of the board or if there are no other
             # ships already placed on the board. If the condition is not met,
             #  an error will be printed.
             if self.battleship.valid_placement(selection, coord,
-                                               direction, self.player_board):
+                                               orientation, self.player_board):
 
                 # If the ship placement is valid, create an object of
                 # subclasses of battleships and make changes to their attributes
                 ship_choice = self.battleship.get_next_ship(selection)[0]
 
                 if ship_choice == "Aircraft Carrier":
-                    ship_choice = Aircraft_Carrier(coord, direction)
+                    ship_choice = Aircraft_Carrier(coord, orientation)
                 elif ship_choice == "Frigate":
-                    ship_choice = Frigate(coord, direction)
+                    ship_choice = Frigate(coord, orientation)
                 elif ship_choice == "Submarine":
-                    ship_choice = Submarine(coord, direction)
+                    ship_choice = Submarine(coord, orientation)
                 elif ship_choice == "Cruiser":
-                    ship_choice = Cruiser(coord, direction)
+                    ship_choice = Cruiser(coord, orientation)
                 elif ship_choice == "Patrol Boat":
-                    ship_choice = Patrol_Boat(coord, direction)
+                    ship_choice = Patrol_Boat(coord, orientation)
                 else:
                     break
 
@@ -46,56 +45,47 @@ class Player:
                 # Displays the board with updated ships placed
                 clear()
             else:
-                CoordinatesOccupiedError()
+                ShipPlacementError('occupied', coord, orientation)
                 self.ship_placement()
 
-    def get_direction(self):
-        direction = input("Horizontal [h] or Vertical [v]? > ").lower()
+    def get_orientation(self):
+        orientation = input("Horizontal [h] or Vertical [v]? > ").lower()
 
-        if direction != 'v' and direction != 'h':
-            InputError().direction()
-            self.get_direction()
+        if orientation != 'v' and orientation != 'h':
+            InputError('orientation', orientation)
+            self.player_board.print_board(self.player_board.game_board)
+            return self.get_orientation()
         else:
-            return direction
+            return orientation
 
     def get_coordinates(self):
         coord = input("Coordinates to place the ship (e.g. a1)? > ").lower()
 
-        if len(coord) > 1:
-            try:
-                coord_num = int(coord[1:])
-                if coord[0].upper() in self.player_board.cols:
-                    if coord_num in self.player_board.rows:
-                        return coord
-                    else:
-                        InputError().coordinates()
-                        self.get_coordinates()
-                else:
-                    InputError().coordinates()
-                    self.get_coordinates()
-            except ValueError:
-                InputError().coordinates()
-                self.get_coordinates()
+        coord_valid = self.player_board.validate_coord(coord)
+        if not coord_valid:
+            self.player_board.print_board(self.player_board.game_board)
+            return self.get_coordinates()
         else:
-            InputError().coordinates()
-            self.get_coordinates()
+            return coord_valid
 
     def get_selection(self):
+        self.player_board.print_board(self.player_board.game_board)
         self.battleship.print_ships()
 
         selection = input("Choose the index (e.g. 1)? > ")
 
         try:
             selection = int(selection) - 1
-
-            if selection in list(range(0, len(self.battleship.ship_info))):
-                return int(selection)
-            else:
-                InputError().index_selection()
-                self.get_selection()
         except ValueError:
-            InputError().value()
-            self.get_selection()
+            InputError('value', selection)
+            return self.get_selection()
+        else:
+            if selection not in list(range(0, len(self.battleship.ship_info))):
+                selection = selection + 1
+                InputError('index_selection', selection)
+                return self.get_selection()
+            else:
+                return selection
 
     def __str__(self):
         self.player_board.print_board(self.player_board.game_board)
