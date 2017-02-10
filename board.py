5b32ee5cae4104ac35e8c12d4d1c48e6f3fadb50
Andrew Che <andrew@codeninja55.me>
2017-02-10 14:27:29 +1100

    Added validate_coord method
    - Moved the validation for coordinates to board class to be used by
    other classes as need.

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 from helpers import convert_coord
-
+from errors import InputError
 
 class Board:
     BOARD_SIZE = 10
@@ -10,6 +10,58 @@ class Board:
     HIT = '*'
     SUNK = '#'
 
+    def validate_coord(self, coord):
+        coord = coord.lstrip().rstrip().lower().replace(' ', '')
+
+        if len(coord) <= 1:
+            InputError('coordinates', coord)
+            return False
+        else:
+            if coord[0].isnumeric() and not coord[1].isnumeric():
+                try:
+                    if int(coord[0]) not in self.rows:
+                        InputError('board_boundaries', coord)
+                        return False
+                except ValueError:
+                    InputError('coordinate_int', coord)
+                    return False
+                else:
+                    if coord[1:].upper() not in self.cols:
+                        InputError('board_boundaries', coord)
+                        return False
+                    else:
+                        coord = str(coord[1]) + str(coord[0])
+                        return self.validate_coord(coord)
+            elif coord[0:2].isnumeric():
+                try:
+                    if int(coord[0:2]) not in self.rows:
+                        InputError('board_boundaries', coord)
+                        return False
+                except ValueError:
+                    InputError('coordinate_int', coord)
+                    return False
+                else:
+                    if coord[2:].upper() not in self.cols:
+                        InputError('board_boundaries', coord)
+                        return False
+                    else:
+                        coord = str(coord[2]) + str(coord[0:2])
+                        return self.validate_coord(coord)
+            else:
+                try:
+                    if int(coord[1]) not in self.rows:
+                        InputError('board_boundaries', coord)
+                        return False
+                except ValueError:
+                    InputError('coordinate_int', coord)
+                    return False
+                else:
+                    if coord[0].upper() not in self.cols:
+                        InputError('board_boundaries', coord)
+                        return False
+                    else:
+                        return coord
+
     def place_ship(self, battleship):
         """Gets coordinates from player class and a convert coordinates
         helper function.Uses the battleship object passed in and the
@@ -18,7 +70,7 @@ class Board:
         game_board"""
         x, y = convert_coord(battleship.coord)
 
-        if battleship.direction == 'v':
+        if battleship.orientation == 'v':
             if x <= self.BOARD_SIZE - battleship.length:
                 for point in range(0, battleship.length):
                     # Place a tuple of coordinates into the positions list
@@ -49,6 +101,7 @@ class Board:
         for row in board:
             print(str(row_num).rjust(2) + " " + (" ".join(row)))
             row_num += 1
+        print("\n")
 
     def __init__(self):
         self.cols = [chr(c) for c in range(ord('A'), ord('A') +
